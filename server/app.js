2c36490fae01704579559d12d44c2b1c811b6dd3
doreenkam <dkamushinda@gmail.com>
2022-04-20 12:20:05 +0100

    Updated / route

--- server/app.js
+++ server/app.js
@@ -1,13 +1,28 @@
 // Creating the server listening at port 3000
 
-const client = require("./dbConnection");
+const db = require("./dbConfig");
 const express = require("express");
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Express departing from port ${port}!`);
+  console.log(`Express departing from  http://localhost:${port} :)`);
 });
 
-client.connect();
+db.connect();
+
+app.get("/", (req, res) => {
+  let intro =
+    "<h2>Here's an app where you can express your feelings anonymously!</h2>";
+  res.send(intro);
+});
+
+app.get("/users", (req, res) => {
+  db.query(`Select * FROM users`, (err, result) => {
+    if (!err) {
+      res.send(result.rows);
+    }
+  });
+  db.end;
+});
