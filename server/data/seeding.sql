90f97758079efb460484d0a529a7f6bbf27fa21e
doreenkam <dkamushinda@gmail.com>
2022-04-20 18:21:07 +0100

    Modified seeding.js

--- server/data/seeding.sql
+++ server/data/seeding.sql
@@ -1,2 +1,12 @@
 const db = require('./dbConfig');
 const fs = require('fs');
+
+const seedQuery = fs.readFileSync(__dirname + '/seedData.sql').toString();
+
+db.query(seedQuery, [hash], (err) => {
+  if (err) {
+    throw err;
+  } else {
+    console.log("Dev database seeded");
+  }
+});
